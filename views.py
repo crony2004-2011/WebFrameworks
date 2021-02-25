d447214de622faeb590e534b224b7c344c4a3282
AS <77235804+crony2004-2011@users.noreply.github.com>
2021-02-26 01:05:33 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import player , team
 # Create your views here.
+
 def index(request):
-    return HttpResponse("<h1>Write this</h1>")
+    return HttpResponse("<h1>Good!!!! Index function Works!!!!!</h1>")
+
+def contact(request):
+    all = player.objects.all()
+    html = ''
+    for i in all:
+        url = str(i.playername) + "-" + str(i.position)
+        html += "<li>" + url + "</li>"
+    return HttpResponse(html)

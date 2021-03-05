7fe7023e139c27f8c39769d91099787fbba8850e
AS <77235804+crony2004-2011@users.noreply.github.com>
2021-03-05 17:39:23 +0530

    Contains all the models

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
+# If we add any new class , attribute or function within this page ,we need to go to the cmd and write python manage.py makemigrations and python manage.py migrate
+
 class team(models.Model):
     name = models.CharField(max_length=1000)
     location = models.CharField(max_length=500)
@@ -9,3 +11,16 @@ class player(models.Model):
     playerid = models.ForeignKey(team, on_delete=models.CASCADE)
     playername = models.CharField(max_length=500)
     position = models.CharField(max_length=500)
+
+class grounds(models.Model):
+    groundname = models.CharField(max_length=500)
+    groundimage = models.TextField()
+
+    def __str__(self):
+        return self.groundname
+
+
+
+
+
+
